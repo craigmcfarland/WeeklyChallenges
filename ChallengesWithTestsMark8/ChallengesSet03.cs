14da3090966428428f5ff042384dd1ec704678d7
Craig McFarland <craigtmcfarland@gmail.com>
2021-03-01 11:07:22 -0500

    set4

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -15,24 +15,55 @@ namespace ChallengesWithTestsMark8
 
         public object IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            var sum = numbers.Sum();
+            return (sum % 2 != 0);
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            var isLower = false;
+            var isUpper = false;
+            var isNumber = false;
+            foreach (var i in password)
+            {
+                if (char.IsLower(i))
+                {
+                    isLower = true;
+                }
+                if (char.IsUpper(i))
+                {
+                    isUpper = true;
+                }
+                if (char.IsNumber(i))
+                {
+                    isNumber = true;
+                }
+            }
+            if (isLower == true && isUpper == true && isNumber == true)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
 
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            return val[0];
-
+            //return val[0];
+            return val.First();
         }
         public char GetLastLetterOfString(string val)
         {
-            var last = val;
-            return last[val.Length - 1];
+            //\var last = val;
+            return val[val.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
@@ -56,17 +87,26 @@ namespace ChallengesWithTestsMark8
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    list.Add(i);
+                }
+
+            }
+            return list.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
 
-        private bool ToUpperCase(string[] words)
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,37 +1,55 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet04
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+
+            return numbers.Where(x => x % 2 == 0).Sum() - numbers.Where(x => x % 2 != 0).Sum();
+
+           /* var sum = 0;
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+                if (num % 2 != 0)
+                {
+                    sum -= num;
+                }
+
+            }
+            return sum;*/
 
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var list = new List<int> { str1.Length, str2.Length, str3.Length, str4.Length };
+            return list.Min();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            
-            {
-
-                throw new NotImplementedException();
-            }
+            var list = new List<int> { number1, number2, number3, number4 };
+            return list.Min();
+           
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return (sideLength1 + sideLength2 > sideLength3 &&
+               sideLength1 + sideLength3 > sideLength2 &&
+               sideLength2 + sideLength3 > sideLength1);
         }
 
         public bool IsStringANumber(string input)
